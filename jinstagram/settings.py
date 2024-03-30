8a5e9a81edc5c7488d4a078d224aee1861f70689
jangwanjung <tjdrj530@naver.com>
2024-03-30 22:28:55 +0900

    error solve img error

--- jinstagram/settings.py
+++ jinstagram/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'jinstagram',
     'content',
     'user',
+
+
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/' # 항상 / 로 끝나도록 설정
+# MEDIA_URL = 'http://static.myservice.com/media/' 다른 서버로 media 파일 복사시
+
+# 업로드된 파일을 저장할 디렉토리 경로
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
